ce8effc3c9832927a20f533091d19b42c9a19434
SirPanda88 <ericdz@live.unc.edu>
2022-11-30 19:07:40 -0500

    default roll

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ import express from "express"
 import { roll } from "./lib/roll.js"
 
 const args = minimist(process.argv.slice(2), {
-    default: {
-        port: 5000
-    },
+	default: {
+		port: 5000
+	},
 });
 
 const app = express();
 
 app.get('/app/', function (req, res) {
-   res.send('200 OK');
+	res.send('200 OK');
 })
 
 app.get('/app/roll/', (req, res) => {
@@ -19,15 +19,21 @@ app.get('/app/roll/', (req, res) => {
 });
 
 
+app.post('/app/roll', (req, res) => {
+	const sides = parseInt(req.body.sides)
+	const dice = parseInt(req.body.dice)
+	const rolls = parseInt(req.body.rolls)
 
+	res.send(roll(sides, dice, rolls))
+})
 
 
 
 
 app.use((req, res) => {
-    res.status(404).send("404 NOT FOUND")
+	res.status(404).send("404 NOT FOUND")
 })
 
 app.listen(args.port, function () {
-  console.log("Server listening on port %s", args.port);
+	console.log("Server listening on port %s", args.port);
 });
