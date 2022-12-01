4fbeec287fde928a1d3317e76cc8881d0c2d890c
SirPanda88 <ericdz@live.unc.edu>
2022-11-30 20:47:41 -0500

    more endpoints

--- server.js
+++ server.js
@@ -33,6 +33,16 @@ app.get('/app/roll/:sides/', (req, res) => {
 	res.send(roll(sides, 2, 1))
 })
 
+app.get('/app/roll/:sides/:dice', (req, res) => {
+    const sides = parseInt(req.params.sides)
+    const dice = parseInt(req.params.dice)
+    res.send(roll(sides, dice, 1))
+})
+
+
+
+
+
 
 app.use((req, res) => {
 	res.status(404).send("404 NOT FOUND")
