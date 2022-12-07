c2f3e87e5954303990b595c0bd782705f561e90d
SirPanda88 <ericdz@live.unc.edu>
2022-12-07 18:07:54 -0500

    cleanup

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/app/roll/', (req, res) => {
 
 
 app.post('/app/roll/', (req, res) => {
-	const sides = parseInt(req.body.sides)
+	const sides = parseInt(req.body.sides;)
 	const dice = parseInt(req.body.dice);
 	const rolls = parseInt(req.body.rolls);
 	console.log(req.body);
