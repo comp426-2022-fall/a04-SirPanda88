23fa6bb414c528199f7df1ca4a58dbf777c60980
SirPanda88 <ericdz@live.unc.edu>
2022-12-07 18:08:39 -0500

    cleanup

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
 })
 
 app.use((req, res) => {
-	res.status(404).send('404 NOT FOUND')
+	res.status(404).send('404 NOT FOUND');
 })
 
 app.listen(args.port, () => {
