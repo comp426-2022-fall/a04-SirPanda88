25e9490affd290b44bfee563dd637646a9d9711b
SirPanda88 <ericdz@live.unc.edu>
2022-12-07 18:05:40 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import minimist from 'minimist'
 import express from 'express'
-import { roll } from "./lib/roll.js"
+import { roll } from './lib/roll.js'
 
 const args = minimist(process.argv.slice(2), {
 	default: {
